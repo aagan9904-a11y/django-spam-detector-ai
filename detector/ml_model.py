21129bb3ebdf0c0ed752e100585ee95a8cf7cc8a
AGAN AGAN <aagan9904@gmail.com>
2026-06-04 18:02:46 +0300

    Add Django dependencies

--- detector/ml_model.py
+++ detector/ml_model.py
@@ -1,11 +1,18 @@
+import os
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
+# Get project root directory
+BASE_DIR = os.path.dirname(
+    os.path.dirname(os.path.abspath(__file__))
+)
+
+csv_path = os.path.join(BASE_DIR, "spam.csv")
+
 # Load dataset
-df = pd.read_csv("spam.csv", encoding='latin-1')
+df = pd.read_csv(csv_path, encoding="latin-1")
 
 df = df[['v1', 'v2']]
 df.columns = ['label', 'message']
@@ -18,21 +25,13 @@ df['label'] = df['label'].map({
 X = df['message']
 y = df['label']
 
-# Vectorization
 vectorizer = CountVectorizer()
-
 X_vectorized = vectorizer.fit_transform(X)
 
-# Train model
 model = MultinomialNB()
-
 model.fit(X_vectorized, y)
 
-# Prediction function
 def predict_spam(message):
-
     vector = vectorizer.transform([message])
-
     prediction = model.predict(vector)
-
     return prediction[0]
